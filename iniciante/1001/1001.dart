f3e8a1f33579ae70925d97a591389f0c556cf462
André Marc <andreluis0015@gmail.com>
2022-04-07 01:19:27 -0300

    + null-safety

--- iniciante/1001/1001.dart
+++ iniciante/1001/1001.dart
@@ -1,8 +1,21 @@
 import 'dart:io';
 
+// RESPOSTA DO BEECROWD (SEM NULL-SAFETY)
+/*
 void main() {
   int a = int.parse(stdin.readLineSync());
   int b = int.parse(stdin.readLineSync());
+
+  int soma = a + b;
+
+  print("X = $soma");
+}
+*/
+
+// RESPOSTA COM NULL-SAFETY
+void main() {
+  int a = int.parse(stdin.readLineSync()!);
+  int b = int.parse(stdin.readLineSync()!);
 
   int soma = a + b;
 
--- iniciante/1002/1002.dart
+++ iniciante/1002/1002.dart
@@ -1,7 +1,20 @@
 import 'dart:io';
 
+// RESPOSTA DO BEECROWD (SEM NULL-SAFETY)
+/*
 void main() {
   double raio = double.parse(stdin.readLineSync());
+  double pi = 3.14159;
+
+  double area = pi * raio * raio;
+
+  print("A=${area.toStringAsFixed(4)}");
+}
+*/
+
+// RESPOSTA COM NULL-SAFETY
+void main() {
+  double raio = double.parse(stdin.readLineSync()!);
   double pi = 3.14159;
 
   double area = pi * raio * raio;
